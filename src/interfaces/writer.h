54b6e9603035adb8ac2975d14cfbb10c593c5df2
jakedern <jakedern@yahoo.com>
2018-10-10 16:59:04 -0500

    first pass at writer interface

---
+++ src/interfaces/writer.h
@@ -0,0 +1,17 @@
+#ifndef _WRITER
+#define _WRITER
+
+#include <stdio.h>
+#include "../types/synchronized_queue.h"
+
+/**
+ * Instructs the writer to begin writing strings to
+ * the provided output file, as they become available
+ * in the queue
+ *
+ * @param f the file to write to
+ * @param q the queue which holds the strings 
+**/
+extern void writeStrings(FILE *f, Queue *q);
+
+#endif
