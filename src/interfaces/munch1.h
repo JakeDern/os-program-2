48ed4c8f9bf66ef28279387d4c83b4dc1e9f1765
jakedern <jakedern@yahoo.com>
2018-10-10 17:01:56 -0500

    munch one interface

---
+++ src/interfaces/munch1.h
@@ -0,0 +1,16 @@
+#ifndef _MUNCHONE
+#define _MUNCHONE
+
+#include "../types/synchronized_queue.h";
+
+/**
+ * Begins processing strings from the incoming queue
+ * and feeding them into the outgoing queue for 
+ * continuous processing
+ *
+ * @param in pointer to the queue holding incoming data
+ * @param out where to put processed strings
+**/
+extern void munchOne(Queue *in, Queue *out);
+
+#endif
