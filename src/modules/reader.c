70e3183ee613f754f93288ba5e96153d241d669e
Jake Dern <jakedern@yahoo.com>
2018-10-15 15:05:28 -0500

    read stdin

--- src/modules/reader.c
+++ src/modules/reader.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  
+  feedInput(NULL, 50);
 }
 
 void feedInput(Queue *q, int buffSize) {
-  
+  char buff[buffSize];
+  int count = 0;
+  while(fgets(buff, buffSize, stdin) != NULL) {
+    printf("line %d: %s", count, buff);
+    count++;
+  }
 }
 
