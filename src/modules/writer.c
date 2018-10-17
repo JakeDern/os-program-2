cfec5e36dc38bfee34906f68a832b755da5bb409
Jake Dern <jakedern@yahoo.com>
2018-10-17 16:02:33 -0500

    commented out mains, makefile, fixed alex's abhorrent style/bugs

--- src/modules/writer.c
+++ src/modules/writer.c
@@ -1,24 +1,16 @@
+#include "../types/synchronized_queue.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 
 extern void writeStrings(Queue *q) {
-
 	char* string;
 
 	while (1) {
-
 		string = DequeueString(q);
 		printf("%s\n", string);
 		free(string);
-	
-
 	}
-
-
-
-
-
 }
 
 
