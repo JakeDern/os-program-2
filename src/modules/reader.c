9de92b03da1eb26d23826abc8dc63571b0ecde10
Jake Dern <jakedern@yahoo.com>
2018-10-17 10:44:51 -0500

    reader

--- src/modules/reader.c
+++ src/modules/reader.c
@@ -8,7 +8,7 @@ void flushInput();
 
 int main(int argc, char **argv) {
   // Queue *q = CreateStringQueue(10);
-  feedInput(NULL, 50);
+  feedInput(NULL, 10);
 }
 
 /** @override */
@@ -22,46 +22,41 @@ void feedInput(Queue *q, int buffSize) {
     char c = getchar();
     charCount++;
 
-    if(charCount == buffSize + 1) {
+    // no room left
+    if(charCount == buffSize) {
+      // flush input
       printf("Input too long, flushing stdin \n");
-      flushInput();
+      while ( (c = getchar()) != '\n' && c != EOF) {}
+      if (c == EOF) { break; }
       charCount = 0;
+
+      // free buff and allocate new string
       free(buff);
       if ( (buff = malloc(sizeof(char) * buffSize)) == NULL ) { exit(1); }
     } else {
+      buff[charCount - 1] = c;
+      // newline indicates more strings to come
       if(c == '\n') {
         buff[charCount] = '\0';
-        // Printing out with %s doesnt work
-        printf("line %d: %s\n", lineCount, buff);
-        // but checking manually, all of the chars are there
-        for(int i = 0; i < charCount; i++) {
-          printf("%c", buff[i]);
-        }
-        printf("\n");
+        printf("line %d: %s", lineCount, buff);
         // TODO enqueue
         charCount = 0;
         lineCount++;
+
         free(buff);
         if ( (buff = malloc(sizeof(char) * buffSize)) == NULL ) { exit(1); }
+      // EOF indicates no more strings
       } else if (c == EOF) {
+        // TODO decide to overrite EOF with null terminator, or
+        // include it in the string
         buff[charCount] = '\0';
         printf("line %d: %s\n", lineCount, buff);
+
         free(buff);
         break;
-      } else {
-        buff[charCount] = c;
-        // printf("%c\n", buff[charCount]);
       }
     }
   }
   // TODO enque null pointer
-}
-
-/**
- * Flushes the remaining characters from stdin
- * */
-void flushInput() {
-  char next;
-  while ( (next = getchar()) != '\n' && next != EOF) {}
 }
 
