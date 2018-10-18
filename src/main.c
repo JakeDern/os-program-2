a3b3ecda29c42e91cd9d4d421515e3a6dee96ac5
Jake Dern <jakedern@yahoo.com>
2018-10-18 12:34:09 -0500

    test main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,25 @@
 #include "./interfaces/writer.h"
 #include "./types/synchronized_queue.h"
 
+const int BUFF_SIZE = 50;
+const int QUEUE_SIZE = 10;
+
 int main(int argc, char **argv) {
-  feedInput(NULL, 50);
+  Queue *in = CreateStringQueue(QUEUE_SIZE);
+  Queue *out = CreateStringQueue(QUEUE_SIZE);
+
+  feedInput(in, BUFF_SIZE);
+
+  // for(int i = 0; i < 8; i++) {
+  //   char *s = DequeueString(in);
+  //   if (s) {
+  //     printf("%s\n", s);
+  //   } else {
+  //     printf("END\n");
+  //   }
+  // }
+
+  munchOne(in , out);
+  munchTwo(out, in);
+  writeStrings(in);
 }
