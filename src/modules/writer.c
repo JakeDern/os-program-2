a0283a319000d49463ff73eb96902a96e1b07e92
Jake Dern <jakedern@yahoo.com>
2018-10-19 13:34:07 -0500

    log strings

---
+++ src/modules/writer.c
@@ -0,0 +1,25 @@
+/**
+ * @author Jake Dern netid: dern2 cslogin: dern
+ * @author Alex Dallman netid: adallman2 cslogin: adallman
+ */
+
+#include "../types/synchronized_queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/** @override */
+void writeStrings(Queue *q) {
+	int cnt = 0;
+	char* string;
+	while (1) {
+		string = DequeueString(q);
+
+		if(string == NULL) {
+			fprintf(stderr, "String processed: %d\n", cnt);
+			pthread_exit(0);
+		}
+		cnt++;
+		printf("%s\n", string);
+		free(string);
+	}
+}
