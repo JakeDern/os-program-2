c33fba1a705961fe2c14a1bef8fc6681724a3773
Jake Dern <jakedern@yahoo.com>
2018-10-19 13:33:19 -0500

    log processed strings

--- src/modules/writer.c
+++ src/modules/writer.c
@@ -9,14 +9,16 @@
 
 /** @override */
 void writeStrings(Queue *q) {
+	int cnt = 0;
 	char* string;
 	while (1) {
 		string = DequeueString(q);
 
 		if(string == NULL) {
+			fprintf(stderr, "String processed: %d\n", cnt);
 			pthread_exit(0);
 		}
-
+		cnt++;
 		printf("%s\n", string);
 		free(string);
 	}
